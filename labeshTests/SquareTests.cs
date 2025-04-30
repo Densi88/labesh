82b1d4ba93f7e975007bc2aa9d63f6a3e0cfd6c4
Denis <ignatiev1904.ph@mail.ru>
2025-04-30 09:22:04 +0800

    Добавил еще тестов для отчета

--- labeshTests/SquareTests.cs
+++ labeshTests/SquareTests.cs
@@ -11,7 +11,7 @@ namespace labesh.Tests
     [TestClass()]
     public class SquareTests
     {
-        
+
         [TestMethod()]
         public void ToTest()
         {
@@ -25,13 +25,13 @@ namespace labesh.Tests
         {
             var m = new Square(100, Square.measure.mSquare);
             var km = new Square(1, Square.measure.gectar);
-            Assert.AreEqual("10100 м^2", (m+km).verbose());
-            
+            Assert.AreEqual("10100 м^2", (m + km).verbose());
+
         }
         [TestMethod()]
         public void SquarePlusTest2()
         {
-            var a=new Square(5, Square.measure.mSquare);
+            var a = new Square(5, Square.measure.mSquare);
             var b = new Square(5, Square.measure.mSquare);
             var result = a + b;
             Assert.AreEqual("10 м^2", result.verbose());
@@ -51,7 +51,7 @@ namespace labesh.Tests
         {
             var a = new Square(5, Square.measure.mSquare);
             var b = new Square(5, Square.measure.mSquare);
-            var result=Square.compare(a, b);
+            var result = Square.compare(a, b);
             Assert.AreEqual("Площади равны!", result);
         }
         [TestMethod()]
@@ -59,8 +59,62 @@ namespace labesh.Tests
         {
             var a = new Square(5, Square.measure.gectar);
             var b = new Square(5, Square.measure.mSquare);
-            var result = Square.compare(a,b);
+            var result = Square.compare(a, b);
             Assert.AreEqual("Первая площадь больше второй!", result);
         }
+
+        [TestMethod()]
+        public void GaDesPlusTest()
+        {
+            var a = new Square(1, Square.measure.gectar);
+            var b = new Square(1, Square.measure.tens);
+            var result = a + b;
+            Assert.AreEqual("2,0925000000000002 гк", result.verbose());
+        }
+
+        [TestMethod()]
+        public void GaSotPlusTest()
+        {
+            var a = new Square(1, Square.measure.gectar);
+            var b = new Square(1, Square.measure.hundred);
+            var result = a + b;
+            Assert.AreEqual("1,01 гк", result.verbose());
+        }
+
+        [TestMethod()]
+        public void GaMetrMinusTest()
+        {
+            var a = new Square(5, Square.measure.gectar);
+            var b = new Square(5, Square.measure.mSquare);
+            var result = a - b;
+            Assert.AreEqual("4,9995 гк", result.verbose());
+        }
+
+        [TestMethod()]
+        public void GaDesMinusTest()
+        {
+            var a = new Square(5, Square.measure.gectar);
+            var b = new Square(1, Square.measure.tens);
+            var result = a - b;
+            Assert.AreEqual("3,9074999999999998 гк", result.verbose());
+        }
+
+        public void GaSotMinusTest()
+        {
+            var a = new Square(5, Square.measure.gectar);
+            var b = new Square(5, Square.measure.hundred);
+            var result = a - b;
+            Assert.AreEqual("4,95 гк", result.verbose());
+        }
+
+        [TestMethod()]
+        public void CompareTest3()
+        {
+            var a = new Square(5, Square.measure.gectar);
+            var b = new Square(5, Square.measure.mSquare);
+            var result = Square.compare(b, a);
+            Assert.AreEqual("Вторая площадь больше первой!", result);
+        }
+
     }
 }
